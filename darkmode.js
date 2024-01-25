7f08a8fa050ef2a41ef3af4e6e2a195ff139ca8c
Marcel Tanuri <marcel.tanuri@gmail.com>
2024-01-24 22:31:37 -0300

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,4 @@
+if (window.location.pathname.startsWith("/group/control_panel/manage")) {
+  var elementoHTML = document.documentElement;
+  elementoHTML.style.filter = "invert(100%) hue-rotate(180deg)";
+}
